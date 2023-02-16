63f3a815494dc1dd0837890f10ef8e8bf6437c04
OBAID ULLAH KHAN <obaidullahkhankhail@gmail.com>
2023-02-16 12:05:59 +0500

    HTTP requests and responses in XML format

--- main.go
+++ main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	"encoding/xml"
+
 	"github.com/gin-gonic/gin"
 )
 
-func IndexHandler(c *gin.Context){
-	name := c.Params.ByName("name")
-	c.JSON(200, gin.H{
-		"message":"hello "+ name,
-	})
+func IndexHandler(c *gin.Context) {
+	// name := c.Params.ByName("name")
+	// c.JSON(200, gin.H{
+	// 	"message":"hello "+ name,
+	// })
+	c.XML(200, Person{FirstName: "Obaid Ullah",
+		LastName: "Khan"})
 }
 
-//main function
+type Person struct {
+	XMLName   xml.Name `xml:"person"`
+	FirstName string   `xml:"firstName,attr"`
+	LastName  string   `xml:"lastName,attr"`
+}
+
+// main function
 func main() {
 	router := gin.Default()
-	router.GET("/:name", IndexHandler)
+	router.GET("/", IndexHandler)
 	router.Run()
 }
